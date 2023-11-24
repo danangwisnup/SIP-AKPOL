1c4c2f2f49102fb04e024f6c8ce331a7f918deeb
danangwisnup <danangwisnu@students.undip.ac.id>
2023-11-24 10:46:16 +0700

    fix model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,38 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The "booting" function of model
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $type = 'USR';
+                $uuid = Str::uuid()->toString();
+                $uuid_second = substr($uuid, 9, 4);
+                $uuid_third = substr($uuid, 14, 4);
+                $uuid_fourth = substr($uuid, 19, 4);
+                $uuid_fifth = substr($uuid, 24, 12);
+                $no = str_pad(User::whereDate('created_at', date('Y-m-d'))->count() + 1, 7, '0', STR_PAD_LEFT);
+                $uid = $type . '.' . $uuid_fifth . $uuid_second . $uuid_third . $uuid_fourth . '.' . $no;
+                $model->{$model->getKeyName()} = $uid;
+            }
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +42,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'uuid',
         'name',
+        'username',
         'email',
         'password',
+        'role',
     ];
 
     /**
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -13,7 +15,18 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $type = 'USR';
+        $uuid = Str::uuid()->toString();
+        $uuid_second = substr($uuid, 9, 4);
+        $uuid_third = substr($uuid, 14, 4);
+        $uuid_fourth = substr($uuid, 19, 4);
+        $uuid_fifth = substr($uuid, 24, 12);
+        $no = str_pad(User::whereDate('created_at', date('Y-m-d'))->count() + 1, 7, '0', STR_PAD_LEFT);
+        $uid = $type . '.' . $uuid_fifth . $uuid_second . $uuid_third . $uuid_fourth . '.' . $no;
+
         DB::table('users')->insert([
+            'uuid' => 'USR.6677e7359c2f075b4dba83c3.0000001',
             'name' => 'Administrator',
             'username' => 'admin',
             'email' => 'admin@localhost.com',
