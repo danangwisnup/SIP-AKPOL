899500b4400b53002676493c972b98c81de8003c
danangwisnup <danangwisnu@students.undip.ac.id>
2023-11-25 09:08:40 +0700

    update semester model

--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -11,6 +11,25 @@ class Semester extends Model
     use HasFactory;
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    public static function uuid()
+    {
+        $type = 'SMT';
+        $uuid = Str::uuid()->toString();
+        $uuid_second = substr($uuid, 9, 4);
+        $uuid_third = substr($uuid, 14, 4);
+        $uuid_fourth = substr($uuid, 19, 4);
+        $uuid_fifth = substr($uuid, 24, 12);
+        $no = str_pad(Semester::whereDate('created_at', date('Y-m-d'))->count() + 1, 7, '0', STR_PAD_LEFT);
+        $uid = $type . '.' . $uuid_fifth . $uuid_second . $uuid_third . $uuid_fourth . '.' . $no;
+
+        return $uid;
+    }
+
+    /**
      * The "booting" function of model
      *
      * @return void
@@ -19,16 +38,7 @@ class Semester extends Model
     {
         parent::boot();
         static::creating(function ($model) {
-            $type = 'SMT';
-            $uuid = Str::uuid()->toString();
-            $uuid_second = substr($uuid, 9, 4);
-            $uuid_third = substr($uuid, 14, 4);
-            $uuid_fourth = substr($uuid, 19, 4);
-            $uuid_fifth = substr($uuid, 24, 12);
-            $no = str_pad(Semester::whereDate('created_at', date('Y-m-d'))->count() + 1, 7, '0', STR_PAD_LEFT);
-            $uid = $type . '.' . $uuid_fifth . $uuid_second . $uuid_third . $uuid_fourth . '.' . $no;
-
-            $model->uuid = $uid;
+            $model->uuid = Semester::uuid();
         });
     }
 
