6a9d396cb5093df03f09521dc20a25b59ae47b32
danangwisnup <danangwisnu@students.undip.ac.id>
2023-11-25 09:08:25 +0700

    update userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,23 +15,24 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-
-        $type = 'USR';
-        $uuid = Str::uuid()->toString();
-        $uuid_second = substr($uuid, 9, 4);
-        $uuid_third = substr($uuid, 14, 4);
-        $uuid_fourth = substr($uuid, 19, 4);
-        $uuid_fifth = substr($uuid, 24, 12);
-        $no = str_pad(User::whereDate('created_at', date('Y-m-d'))->count() + 1, 7, '0', STR_PAD_LEFT);
-        $uid = $type . '.' . $uuid_fifth . $uuid_second . $uuid_third . $uuid_fourth . '.' . $no;
-
-        DB::table('users')->insert([
-            'uuid' => 'USR.6677e7359c2f075b4dba83c3.0000001',
-            'name' => 'Administrator',
-            'username' => 'admin',
-            'email' => 'admin@localhost.com',
-            'password' => bcrypt('admin'),
-            'role' => 'admin',
+        $data = ([
+            [
+                'name' => 'Administrator',
+                'username' => 'admin',
+                'email' => 'admin@localhost.com',
+                'password' => bcrypt('admin'),
+                'role' => 'admin',
+            ], [
+                'name' => 'Danton 1',
+                'username' => 'Danton 1',
+                'email' => 'danton1@localhost.com',
+                'password' => bcrypt('danton1'),
+                'role' => 'danton',
+            ]
         ]);
+
+        foreach ($data as $key => $value) {
+            User::create($value);
+        }
     }
 }
